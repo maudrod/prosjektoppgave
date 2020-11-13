208d12d2fa98e7f2b80fcfa2ebc0fa8a2d20f472
Emil Alvar Myhre <emilam@dhcp-10-22-40-155.wlan.ntnu.no>
2020-11-13 20:25:08 +0100

    tauloglikes binsizes

---
+++ BinsizeTests/Plotting.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 13 20:11:10 2020
+
+@author: emilam
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+Taus = np.load('Taus.npy')
+w0ests2ms = np.load('w0estimates_2ms.npy')
+w0ests5ms = np.load('w0estimates_5ms.npy')
+
+
+loglikesTau2ms = np.load('loglikes_2ms.npy')
+loglikesTau5ms= np.load('Loglikes_5ms.npy')
+
+indexes = np.linspace(0,len(loglikesTau2ms)-1,len(loglikesTau2ms))
+
+w0highs2ms = np.where(w0ests2ms > 1.1)
+w0lows2ms = np.where(w0ests2ms < 0.9)
+
+w0highs5ms = np.where(w0ests5ms > 1.1)
+w0lows5ms = np.where(w0ests5ms<0.9)
+
+indexesgood2ms = np.delete(indexes,np.concatenate((w0highs2ms[0],w0lows2ms[0])))
+
+indexesgood5ms = np.delete(indexes,np.concatenate((w0highs5ms[0],w0lows5ms[0])))
+
+
+Tau2msgoods = []
+for i in range(len(indexesgood2ms)):
+    Tau2msgoods.append(loglikesTau2ms[indexesgood2ms[i].astype(int)])
+    
+Tau2mshighs = []
+for i in range(len(w0highs2ms[0])):
+    Tau2mshighs.append(loglikesTau2ms[w0highs2ms[0][i].astype(int)])
+
+
+Tau2mslows = []
+
+for i in range(len(w0lows2ms[0])):
+    Tau2mslows.append(loglikesTau2ms[w0lows2ms[0][i].astype(int)])
+
+Tau5msgoods = []
+
+for i in range(len(indexesgood5ms)):
+    Tau5msgoods.append(loglikesTau5ms[indexesgood5ms[i].astype(int)])
+
+    
+Tau5mshighs = []
+for i in range(len(w0highs5ms[0])):
+    Tau5mshighs.append(loglikesTau5ms[w0highs5ms[0][i].astype(int)])
+    
+Tau5mslows = []
+for i in range(len(w0lows5ms[0])):
+    Tau5mslows.append(loglikesTau5ms[w0lows5ms[0][i].astype(int)])
+    
+plt.figure()
+plt.title('Tau loglikelihood - Good estimate w0 - 2ms')
+plt.xlabel('Tau')
+plt.ylabel('Loglikelihood')
+plt.plot(Taus,np.asarray(Tau2msgoods).mean(0),label='Mean 100 datasets')
+plt.axvline(0.02,color='r',linestyle='--',label='True Value')
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.title('Tau loglikelihood - Good estimate w0 - 5ms')
+plt.xlabel('Tau')
+plt.ylabel('Loglikelihood')
+plt.plot(Taus,np.asarray(Tau5msgoods).mean(0),label='Mean 100 datasets')
+plt.axvline(0.02,color='r',linestyle='--',label='True Value')
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.title('Tau loglikelihood - High estimate w0 - 2ms')
+plt.xlabel('Tau')
+plt.ylabel('Loglikelihood')
+plt.plot(Taus,np.asarray(Tau2mshighs).mean(0),label='Mean 100 datasets')
+plt.axvline(0.02,color='r',linestyle='--',label='True Value')
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.title('Tau loglikelihood - High estimate w0 - 5ms')
+plt.xlabel('Tau')
+plt.ylabel('Loglikelihood')
+plt.plot(Taus,np.asarray(Tau5mshighs).mean(0),label='Mean 100 datasets')
+plt.axvline(0.02,color='r',linestyle='--',label='True Value')
+plt.legend()
+plt.show()
+    
+plt.figure()
+plt.title('Tau loglikelihood - Low estimate w0 - 2ms')
+plt.xlabel('Tau')
+plt.ylabel('Loglikelihood')
+plt.plot(Taus,np.asarray(Tau2mslows).mean(0),label='Mean 100 datasets')
+plt.axvline(0.02,color='r',linestyle='--',label='True Value')
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.title('Tau loglikelihood - Low estimate w0 - 5ms')
+plt.xlabel('Tau')
+plt.ylabel('Loglikelihood')
+plt.plot(Taus,np.asarray(Tau5mslows).mean(0),label='Mean 100 datasets')
+plt.axvline(0.02,color='r',linestyle='--',label='True Value')
+plt.legend()
+plt.show()
+
