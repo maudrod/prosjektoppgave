f1e06b4e0a23a2c58ab6e1510f08ba58df54a694
Emil Alvar Myhre <emilam@dhcp-10-22-40-155.wlan.ntnu.no>
2020-11-15 14:04:15 +0100

    litt 2ms tau plots

--- BinsizeTests/Plotting.py
+++ BinsizeTests/Plotting.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-
+'''
 Taus = np.load('Taus.npy')
 w0ests2ms = np.load('w0estimates_2ms.npy')
 w0ests5ms = np.load('w0estimates_5ms.npy')
@@ -83,7 +83,7 @@ Tau1mslows = []
 
 for i in range(len(w0lows1ms[0])):
     Tau1mslows.append(loglikesTau1ms[w0lows1ms[0][i].astype(int)])
-'''
+
 plt.figure()
 plt.title('Tau loglikelihood - Good estimate w0 - 2ms')
 plt.xlabel('Tau')
@@ -156,7 +156,7 @@ plt.plot(Taus,np.asarray(Tau1mshighs).mean(0),label='Mean 100 datasets')
 plt.axvline(0.02,color='r',linestyle='--',label='True Value')
 plt.legend()
 plt.show()
-'''
+
 plt.figure()
 plt.title('Tau loglikelihood - Low estimate w0 - 1ms')
 plt.xlabel('Tau')
@@ -165,3 +165,76 @@ plt.plot(Taus,np.asarray(Tau1mslows).mean(0),label='Mean 100 datasets')
 plt.axvline(0.02,color='r',linestyle='--',label='True Value')
 plt.legend()
 plt.show()
+'''
+##### 2MS TAU INFERENCE ######
+
+Tau1 = np.load('Tau0.0001noise2msB275.npy')
+Tau2 = np.load('Tau0.001noise2msB275.npy')
+Tau3 = np.load('Tau0.003noise2msB275.npy')
+Tau4 = np.load('Tau0.005noise2msB275.npy')
+
+
+Tau5 = np.load('Tau0.0001noise2msB31.npy')
+Tau6 = np.load('Tau0.001noise2msB31.npy')
+Tau7 = np.load('Tau0.003noise2msB31.npy')
+Tau8 = np.load('Tau0.005noise2msB31.npy')
+
+Taumean1 = np.mean(Tau1[300:])
+Tauvar1 = np.var(Tau1[300:])
+Taumean2 = np.mean(Tau2[300:])
+Tauvar2 = np.var(Tau2[300:])
+Taumean3 = np.mean(Tau3[300:])
+Tauvar3 = np.var(Tau3[300:])
+Taumean4 = np.mean(Tau4[300:])
+Tauvar4 = np.var(Tau4[300:])
+Taumean5 = np.mean(Tau5[300:])
+
+
+Tauvar5 = np.var(Tau5[300:])
+Taumean6 = np.mean(Tau6[300:])
+Tauvar6 = np.var(Tau6[300:])
+Taumean7 = np.mean(Tau7[300:])
+Tauvar7 = np.var(Tau7[300:])
+Taumean8 = np.mean(Tau7[300:])
+Tauvar8 = np.var(Tau7[300:])
+
+Taumeans275 = [Taumean1,Taumean2,Taumean3,Taumean4]
+Taumeans31 = [Taumean5,Taumean6,Taumean7,Taumean8]
+Taustds275 = [np.sqrt(Tauvar1),np.sqrt(Tauvar2),np.sqrt(Tauvar3),np.sqrt(Tauvar4)] 
+Taustds31 = [np.sqrt(Tauvar5),np.sqrt(Tauvar6),np.sqrt(Tauvar7),np.sqrt(Tauvar8)]
+'''
+x = [1,2,3,4]
+ticksss = ['0.0001','0.001','0.003','0.005']
+plt.figure()
+plt.title('Tau sensitivity of noise - 2ms - $b_1 = b_2$ = -3.1')
+plt.xlabel('Noise')
+plt.ylabel('Tau estimation')
+plt.ylim([-0.06,0.1])
+plt.xlim([0,5])
+plt.xticks(x,labels = ticksss)
+for i in range(4):
+    plt.errorbar(x[i], Taumeans31[i], yerr = Taustds31[i],marker = 'o')
+plt.axhline(0.02,color='r',linestyle='--',label='True Value')
+plt.legend()
+plt.show()
+'''
+
+
+plt.figure()
+sns.displot(Tau1[300:], kde=True)
+plt.axvline(0.02,color='r',linestyle='--',label='True Value')
+plt.xlim([0,0.08])
+plt.legend()
+plt.title('Posterior distribution Tau - 0.0001 noise - 2ms - $b_1 = b_2$ = -2.75')
+plt.show()
+'''
+plt.figure()
+plt.title('Tau inference - 0.005 noise - 2ms - $b_1 = b_2$ = -2.75')
+plt.xlabel('Iteration')
+plt.ylabel('Tau')
+plt.ylim([0,0.08])
+plt.plot(np.linspace(1,1500,1500),Tau8,'ko')
+plt.axhline(0.02,color='r',linestyle='--',label='True Value')
+plt.legend()
+plt.show()
+'''
